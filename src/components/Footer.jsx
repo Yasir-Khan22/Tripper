1e561e2c968bd05b36a9000e16aaee97988c6753
yasir-khan22 <yasirkhanone22@gmail.com>
2023-06-04 22:44:24 +0500

    fix - footer author name issue fixed.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 export default function Footer() {
+
     return (
-        <footer>&#169; 2023 Tripper Made with 💚 by <a href="https://www.github.com/yasir-khan22" target="__blank">YasirKhan</a></footer>
+        <footer>&#169; 2023 Tripper Made with 💚 by<a style={{ padding: "5px" }} href="https://www.github.com/yasir-khan22" target="__blank"> @YasirKhan</a></footer>
     )
 }
