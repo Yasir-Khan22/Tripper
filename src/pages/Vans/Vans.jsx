3e95d07a76f797d0ff7c98ef69e2d985a0a5395f
yasir-khan22 <yasirkhanone22@gmail.com>
2023-06-07 23:32:21 +0500

    feat. filters are added to the vans component.

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,8 +1,24 @@
 import React, { useEffect, useState } from "react"
 import { Link, useSearchParams } from "react-router-dom"
+import styled from "styled-components";
 
 export default function Vans() {
 
+
+    const StyledLink = styled(Link)`
+    margin: 1.4rem 20px;
+    background-color: #bdbdbd;
+    color: #636363;
+    text-decoration: none;
+    border-radius: 10px;
+    padding: 10px 20px;
+    font-weight: bold;
+    font-size: 1.2rem;
+    margin-top: 20px;
+    &:hover {
+        text-decoration-line: underline;
+    }
+    `
     const [searchParams, setSearchParams] = useSearchParams();
 
     const typeFilter = searchParams.get("type")
@@ -20,6 +36,12 @@ export default function Vans() {
     return (
         <div className="van-list-container">
             <h1>Explore our Van's.</h1>
+            <div className="filterBtn" style={{ marginTop: "2rem" }}>
+                <StyledLink to="?type=Simple" className="simpleTag">Simple</StyledLink>
+                <StyledLink to="?type=Rugged" className="ruggedTag">Rugged</StyledLink>
+                <StyledLink to="?type=Luxury" className="luxuryTag">Luxury</StyledLink>
+                <StyledLink to="." className="clear-filters">Clear Filter</StyledLink>
+            </div>
             <div className="van-list">
                 {displayedVans?.map(van => (
                     <div key={van.id} className="van-tile">
