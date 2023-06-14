be7fd8894a71a624d38068afd0f4b2d4ecb46cc8
yasir-khan22 <yasirkhanone22@gmail.com>
2023-06-14 23:11:52 +0500

    doc site footer modified.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
 export default function Footer() {
-
     return (
         <footer>&#169; 2023 Tripper Made with 💚 by<a style={{ padding: "5px", textDecoration: "None", color: "#16c60c" }} href="https://www.github.com/yasir-khan22" target="__blank">Yasir Khan</a></footer>
     )
